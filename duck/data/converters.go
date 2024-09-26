757a1bacac5f00b0cc5b6ca2b1abf2244b4cf6be
Scott Lepper <scott.lepper@gmail.com>
2024-09-26 17:39:26 -0400

    convert string dates to time

--- duck/data/converters.go
+++ duck/data/converters.go
@@ -3,6 +3,7 @@ package data
 import (
 	"time"
 
+	"github.com/araddon/dateparse"
 	"github.com/grafana/grafana-plugin-sdk-go/data"
 	"github.com/grafana/grafana-plugin-sdk-go/data/framestruct"
 )
@@ -49,4 +50,62 @@ func parseDate(s string) (time.Time, error) {
 		return t, err
 	}
 	return t.UTC(), nil
+}
+
+func ConvertDateFields(results []map[string]any) {
+	dateFields := findDateFields(results)
+	for row, result := range results {
+		for key, value := range result {
+			isDateField := dateFields[key]
+			if isDateField != nil && *isDateField {
+				if s, ok := value.(string); ok {
+					dateValue := isDate(s)
+					results[row][key] = dateValue
+				}
+			}
+		}
+	}
+}
+
+func findDateFields(results []map[string]any) map[string]*bool {
+	dateFields := make(map[string]*bool)
+	for _, result := range results {
+		if len(dateFields) == len(result) {
+			break
+		}
+		for key, value := range result {
+			if value == nil {
+				continue
+			}
+
+			isDateField := dateFields[key]
+			if isDateField != nil {
+				continue
+			}
+
+			if s, ok := value.(string); ok {
+				if s == "" {
+					continue
+				}
+
+				dateValue := isDate(s)
+				flag := true
+				flagPtr := &flag
+				if dateValue != nil {
+					dateFields[key] = flagPtr
+				} else {
+					*flagPtr = false
+				}
+			}
+		}
+	}
+	return dateFields
+}
+
+func isDate(s string) *time.Time {
+	val, err := dateparse.ParseStrict(s)
+	if err != nil {
+		return nil
+	}
+	return &val
 }
